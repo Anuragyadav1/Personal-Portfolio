ba2855679716f9673109d1a893f9e4430273b19d
Anurag Yadav <10anuragyadav2002@gmail.com>
2025-08-14 20:58:44 +0530

    Home page content updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,8 @@ const Home = () => {
             <p className='text-[#8892b0] py-4 max-w-[700px] text-lg'>
               {/* I specialize in building exceptional digital experiences. Currently, I'm focused on
               building responsive full-stack web applications that deliver value and solve real-world problems. */}
-              I'm a final-year CSE student at IIIT Sri City, passionate about building full-stack web applications.
+                I recently graduated with a B.Tech in Computer Science and Engineering from IIIT Sri City, 
+                passionate about building full-stack web applications.
             </p>
             
             <div className='flex gap-4 mt-2 justify-center md:justify-start'>
