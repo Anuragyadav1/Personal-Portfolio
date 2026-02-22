af702192dd68597cbfc39ae837e162e5490ff5b0
Anurag Yadav <10anuragyadav2002@gmail.com>
2026-02-22 21:11:48 +0530

    download resume button disabled

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,13 +1,14 @@
-
 import React from 'react';
 import { FaFileDownload } from 'react-icons/fa';
 
 const ResumeButton = () => {
   return (
-    <a 
-      href="/Anurag_Yadav_CV_04.pdf" 
-      download 
-      className="flex items-center px-6 py-3 my-2 border-[#64ffda] text-[#64ffda] hover:bg-[#dae7e3] hover:bg-opacity-10"
+    <a
+      href="/Anurag_Yadav_CV_04.pdf"
+      className="flex items-center px-6 py-3 my-2 border border-[#64ffda] 
+                 text-[#64ffda] opacity-40 cursor-not-allowed 
+                 pointer-events-none"
+      aria-disabled="true"
     >
       <FaFileDownload className="mr-2" />
       Resume
